38e8e910a2ce68144aed1a6c470dacb036f3c9a3
gentlekitten69 <tiahalibel2010@hotmail.com>
2024-01-18 19:28:02 -0500

    test

---
+++ gaming_library/src/Display/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Route, Router, Routes} from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        
+    )
+}
+
+
+
+{/* <header>
+<a href="/" class="logo">Home</a>
+ <nav class="navbar">
+  <ul class="navbar-nav">
+       <li ><a class="nav-link" href= "/Genre.js">Genre</a></li>
+       <li><a href="/gaming_library/src/Function/Popular.js">Popular Games</a></li>
+  </ul>
+ </nav> 
+</header>
+<section class="banner"></section>
+<script type="text/JavaScript">
+  window.addEventListener("scroll", function(){
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  })
+</script> */}
--- gaming_library/src/Function/Popular.js
+++ gaming_library/src/Function/Popular.js
@@ -1,7 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+
 const PopularGames = () => {
+
+    const [games, setGames] = useState([]);
+    const URL = `http://api.rawg.io/api/games?dates=2023-01-01,2023-12-31&ordering=-added&key=ef855fc72b30488f8dc0e80014dbfc6a`
+
+    useEffect(() => {
+        const fetchGames = async () => {
+            try {
+                const response = await fetch(URL);
+                const data = await response.json();
+
+                if (data.results) {
+                   setGames();
+                   
+                }
+            } catch (error) {
+                console.error('Error fetching games:', error);
+            }
+        };
+
+        fetchGames();
+    }, [URL]);
+
     return (
         <div>
-            <h1>Popular Games</h1>
+            
+            <div className='card'style={{ display: 'flex', flexWrap:'wrap', justifyContent: 'space-evenly'}}>
+                        {games.map((game) => (
+                        <Card  key={game.id} sx={{maxwidth:300, 
+                            margin: '16px', backgroundColor:'blueviolet'}}>
+                        <CardMedia
+                            component='img'
+                            alt= {game.name}
+                            sx={{height: 180, maxWidth:400 }}
+                            image={game.background_image} />
+                       <CardContent>
+                            <Typography variant='h6' fontWeight='bold' 
+                                textAlign='center'>
+                                {game.name}
+                            </Typography>
+                        </CardContent>
+                       <CardActions style={{justifyContent:'center'}}>
+                            <Link to={`/GameDetails/${game.id} `}>;
+                            <button>Details</button>
+                            </Link>
+                         </CardActions>      
+                        </Card>
+                         ))}
+                    </div>
         </div>
     )
 }
